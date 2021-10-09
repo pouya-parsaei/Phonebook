5177646c237585a4a5e8002438d341fc27ddee47
pouya-parsaei <pouya.goodboy1371@gmail.com>
2021-10-09 09:02:31 +0330

    StupidRouter-Finished

--- App/Core/StupidRouter.php
+++ App/Core/StupidRouter.php
@@ -2,13 +2,37 @@
 
 namespace App\Core;
 
+use App\Utilities\Url;
+
 class StupidRouter
 {
     private $routes;
     public function __construct()
     {
+
         $this->routes = [
-            
+            '/pouya/blue' => 'colors/blue.php',
+            '/pouya/red' => 'colors/red.php',
+            '/pouya/green' => 'colors/green.php'
         ];
+    }
+
+    public function run()
+    {
+        $currentRoute = Url::currentRoute();
+
+        foreach ($this->routes as $route => $view) {
+            if ($currentRoute == $route) {
+                $this->includeAndDie(BASE_PATH . "views/$view");
+            }
+            header("HTTP/1.1 404 Not Found");
+            $this->includeAndDie(BASE_PATH . "views/errors/404.php");
+        }
+    }
+
+    private function includeAndDie($viewPath)
+    {
+        include $viewPath;
+        die();
     }
 }
--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Utilities\Url;
+use App\Core\StupidRouter;
 
 # front controller
 
 include 'bootstrap/init.php';
 
-$route = Url::currentRoute();
-if ($route == '/pouya/blue')
-    include BASE_PATH . 'views/colors/blue.php';
-
-if ($route == '/pouya/red')
-    include BASE_PATH . 'views/colors/red.php';
-
-if ($route == '/atal/matal/tootoole')
-    include BASE_PATH . 'views/colors/green.php';
+$router = new StupidRouter();
+$router->run();
